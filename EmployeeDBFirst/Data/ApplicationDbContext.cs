97911309c11956d52765c2a0a2adff08845cec04
Mohammed Fananah <Hp@HP>
2023-02-21 13:12:46 +0200

    Add project files.

---
+++ EmployeeDBFirst/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeDBFirst.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeDBFirst.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeDBFirst/Models/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeDBFirst.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="English Name")]
+        public string EmployeeNameEnglish { get; set; }
+        [Required]
+        [Display(Name = "الاسم بالعربية")]
+        public string EmployeeNameArabic { get; set; }
+        [Required]
+        public DateTime Dob { get; set; }
+        [Required]
+        public DateTime HiringDate { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+        [Required]
+        public int? CityId { get; set; }
+        [Required]
+        public int? DepartmentId { get; set; }
+
+        [Required]
+        public virtual City City { get; set; }
+        [Required]
+        public virtual Department Department { get; set; }
+    }
+}
